abd7f54af2709cb72863c5a76e3b4e5505ebb844
Bryan31542 <00063820@uca.edu.sv>
2023-06-02 21:37:53 -0600

    feat: add jwt :closed_lock_with_key:

--- models/server.js
+++ models/server.js
@@ -7,6 +7,7 @@ class Server {
     this.app = express();
     this.port = process.env.PORT;
     this.usersPath = "/api/users";
+    this.authPath = "/api/auth";
 
     this.connectingDB();
 
@@ -30,6 +31,7 @@ class Server {
   }
 
   routes() {
+    this.app.use(this.authPath, require("../routes/auth"));
     this.app.use(this.usersPath, require("../routes/user"));
   }
 
---
+++ routes/auth.js
@@ -0,0 +1,36 @@
+const { Router } = require("express");
+const { check } = require("express-validator");
+
+const { usersPost } = require("../controllers/user");
+
+const { login } = require("../controllers/auth");
+const { validateFields } = require("../middlewares/validate-fields");
+const { emailExist } = require("../helpers/db-validators");
+
+const router = Router();
+
+router.post(
+  "/login",
+  [
+    check("email", "Email is required").isEmail(),
+    check("password", "Password is required").not().isEmpty(),
+    validateFields,
+  ],
+  login
+);
+
+router.post(
+  "/register",
+  [
+    check("name", "name is required").not().isEmpty(),
+    check("email", "Email is not valid").isEmail(),
+    check("email").custom(emailExist),
+    check("password", "password must have more than 8 digits").isLength({
+      min: 8,
+    }),
+    validateFields,
+  ],
+  usersPost
+);
+
+module.exports = router;
